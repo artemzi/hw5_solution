4f571dfe1f658f65ffc549bf99aa7777bedeb33f
Artem Zinoviev <arzinoviev@gmail.com>
2018-03-01 14:57:18 +0300

    finish part, next parse request

--- handlers_gen/codegen.go
+++ handlers_gen/codegen.go
@@ -18,6 +18,8 @@ var (
 import (
 	"net/http"
 	"encoding/json"
+	"log"
+	"io/ioutil"
 )
 `))
 
@@ -37,64 +39,44 @@ func (api *{{.StructName}}) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 `))
 
 	caseTemplate = template.Must(template.New("caseTemplate").Parse(`
-	// method {{.Method}}
 	case "{{.Path}}":
-		api.handler{{.Handler}}(w, r)
+		handler{{.Handler}}(w, r)
 `))
 
-	httpWrapperTemplate = template.Must(template.New("httpWrapperHeaderTemplate").Parse(`
-func (srv *{{.StructName}}) handler{{.FuncName}}(w http.ResponseWriter, r *http.Request) {
-	resp := make(map[string]interface{})
-	resp["error"] = ""
-	{{.Method}}
-	{{.Auth}}
-	var v url.Values
-	switch r.Method {
-	case "POST":
-		v = parseCrutchyBody(r.Body)
-	default:
-		v = r.URL.Query()
-	}
-	var params {{.FuncName}}Params
-	if err := params.validateAndFill{{.FuncName}}Params(v); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		resp["error"] = err.Error()
-		body, _ := json.Marshal(resp)
-		w.Write(body)
-		return
-	}
-	user, err := srv.{{.FuncName}}(r.Context(), params)
+	handlerTemplate = template.Must(template.New("handlerTemplate").Parse(`
+func handlerCreate(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		switch err.(type) {
-		case ApiError:
-			w.WriteHeader(err.(ApiError).HTTPStatus)
-			resp["error"] = err.Error()
-		default:
-			w.WriteHeader(http.StatusInternalServerError)
-			resp["error"] = "bad user"
-		}
-		body, _ := json.Marshal(resp)
-		w.Write(body)
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-	resp["response"] = user
-	body, _ := json.Marshal(resp)
-	w.Write(body)
+	log.Printf("handlerCreate body: %v\n", string(body))
+}
+
+func handlerProfile(w http.ResponseWriter, r *http.Request) {
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+	log.Printf("handlerProfile body: %v\n", string(body))
 }
 `))
 )
 
-type MethodSignature struct {
-	Url    string `json:"url"`
+type apigen struct {
+	URL    string `json:"url"`
 	Auth   bool   `json:"auth"`
 	Method string `json:"method"`
 }
 
 type caseTpl struct {
-	Path      string
-	Handler   string
-	Structure string
-	Method    string
+	Path    string
+	Handler string
+	Method  string
+	Struct  string
 }
 
 type structTpl struct {
@@ -102,24 +84,6 @@ type structTpl struct {
 	Package    string
 	Cases      string
 	Method     string
-}
-
-func getStructures(currFunc *ast.FuncDecl, structures []string) []string {
-	for _, doc := range currFunc.Doc.List {
-		if strings.Contains(doc.Text, "apigen:api") {
-		LOOP:
-			for _, i := range currFunc.Recv.List {
-				structName := i.Type.(*ast.StarExpr).X.(*ast.Ident)
-				for _, i := range structures {
-					if i == structName.Name {
-						break LOOP
-					}
-				}
-				structures = append(structures, structName.Name)
-			}
-		}
-	}
-	return structures
 }
 
 func main() {
@@ -130,14 +94,16 @@ func main() {
 	}
 
 	var (
+		structName *ast.Ident
+		cases      []caseTpl
 		structures []string
-		cases      caseTpl
 	)
 	output := new(bytes.Buffer)
 
 	dest := strings.ToLower(os.Args[2]) // here we can change filename or add some check&whatever
 	out, _ := os.Create(dest)
 
+	tplHeader.Execute(out, structTpl{Package: node.Name.Name}) // add package decklarations and imports
 	for _, n := range node.Decls {
 		currFunc, ok := n.(*ast.FuncDecl)
 		if !ok {
@@ -145,26 +111,42 @@ func main() {
 		}
 
 		if currFunc.Doc != nil {
-			structures = getStructures(currFunc, structures)
-			methodSignature := new(MethodSignature)
-			for _, doc := range currFunc.Doc.List {
-				if strings.Contains(doc.Text, "apigen:api") {
-					start := strings.Index(doc.Text, "{")
-					end := strings.Index(doc.Text, "}") + 1
-					json.Unmarshal([]byte(doc.Text[start:end]), methodSignature)
-					cases = caseTpl{methodSignature.Url, currFunc.Name.Name, "", methodSignature.Method}
+			apigen := &apigen{Method: "GET" /*Default value*/}
+			for _, i := range currFunc.Recv.List {
+				structName = i.Type.(*ast.StarExpr).X.(*ast.Ident)
+
+				for _, doc := range currFunc.Doc.List {
+					if strings.Contains(doc.Text, "apigen:api") {
+						start := strings.Index(doc.Text, "{")
+						end := strings.Index(doc.Text, "}") + 1
+						json.Unmarshal([]byte(doc.Text[start:end]), apigen)
+						cases = append(cases, caseTpl{apigen.URL, currFunc.Name.Name, apigen.Method, structName.Name})
+						if len(structures) > 0 {
+							for _, i := range structures {
+								if i == structName.Name {
+									break
+								}
+								structures = append(structures, structName.Name)
+							}
+						} else {
+							structures = append(structures, structName.Name)
+						}
+					}
 				}
 			}
 		}
 	}
 
-	tplHeader.Execute(out, structTpl{Package: node.Name.Name}) // add package decklarations and imports
-	for _, s := range structures {                             // TODO
-		cases.Structure = s
-		caseTemplate.Execute(output, cases)
+	for _, s := range structures {
+		for _, c := range cases {
+			if c.Struct == s {
+				caseTemplate.Execute(output, c)
+			}
+		}
 		tpl.Execute(out, structTpl{StructName: s, Cases: output.String()})
-		output = new(bytes.Buffer) // clear last assigment
+		output = new(bytes.Buffer)
 	}
+	handlerTemplate.Execute(out, nil)
 	out.Close()
 }
 
